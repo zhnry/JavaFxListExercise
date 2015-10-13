f7b980854705688399818f53b925929f25e58845
hzhen <hzhen@hzhen.ad.corp.appnexus.com>
2015-10-13 10:04:56 -0400

    final result

--- PopulateList/src/populatelist/FXMLDocumentController.java
+++ PopulateList/src/populatelist/FXMLDocumentController.java
@@ -6,7 +6,11 @@
 package populatelist;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -15,6 +19,8 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 /**
  *
