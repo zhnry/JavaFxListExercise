dccd9a90b49176a65c0ba14793457bc09e60e1e6
hzhen <hzhen@hzhen.ad.corp.appnexus.com>
2015-10-13 09:19:30 -0400

    final result

--- PopulateList/src/populatelist/FXMLDocumentController.java
+++ PopulateList/src/populatelist/FXMLDocumentController.java
@@ -6,7 +6,11 @@
 package populatelist;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -15,6 +19,8 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 /**
  *
@@ -43,35 +49,48 @@ public class FXMLDocumentController implements Initializable {
   
   @Override
   public void initialize(URL url, ResourceBundle rb) {
-    // TODO
+    SyncPeopleListView();
   }  
 
     @FXML
     private void handleBtnAddPersonClicked(MouseEvent event) {
-        String fullName = tfFirstName.getText() + " " + tfLastName.getText();
-        System.out.println(fullName);
-        lvPeople.getItems().add(fullName);
+        Person newPerson = new Person();
+        newPerson.setFirstName(tfFirstName.getText());
+        newPerson.setLastName(tfLastName.getText());
+        
+        EntityManagerFactory emf =
+         Persistence.createEntityManagerFactory("PopulateListPU");
+
+        PersonJpaController jpaPerson = new PersonJpaController(emf);
+
+        try {
+                jpaPerson.create(newPerson);
+            }   catch (Exception ex) {
+        Logger.getLogger(FXMLDocumentController.class.getName()).log(
+        Level.SEVERE, null, ex);       
+            }  
     }
     
-    SyncPeopleListView(){
-            // Items inside the list
+    public void SyncPeopleListView() {
+
+    // Items inside the list
     ObservableList<String> items = lvPeople.getItems();
 
     // Clear out the list
     items.clear();
-
+    // Get list of all people in the database
     EntityManagerFactory emf = Persistence.createEntityManagerFactory("PopulateListPU");
-        PersonJpaController jpaPerson;
+    PersonJpaController jpaPerson;
     jpaPerson = new PersonJpaController(emf);
 
     List<Person> people = jpaPerson.findPersonEntities();
-
-    }
     
-        // Add each person to the list
+    // Add each person to the list
     for (Person p : people) {
       String fullName = p.getFirstName() + " " + p.getLastName();
       lvPeople.getItems().add(fullName);
     }
 
+
+    }  
 }
